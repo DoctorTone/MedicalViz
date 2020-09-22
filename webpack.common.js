9b499a6c498f9a6e4a49d9e31574d6ff5535b27c
DoctorTone <drtonesoftware@gmail.com>
2020-09-22 01:15:56 +0100

    Webpack configuration.

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,8 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
     module: {
         rules: [
             {
